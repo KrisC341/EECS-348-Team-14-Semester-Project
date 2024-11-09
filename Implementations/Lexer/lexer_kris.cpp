5f62c8c067d46967134681b0951a1444b5733d74
KrisC341 <142916951+KrisC341@users.noreply.github.com>
2024-11-09 14:11:20 -0600

    created implementation folders
    created implementation folders for different variants of possible component and package implementations

---
+++ Implementations/Lexer/lexer_kris.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+
+using std::string;
+
+enum TokenType {
+    TOKEN_NUMBER,
+    TOKEN_PLUS,
+    TOKEN_MINUS,
+    TOKEN_STAR,
+    TOKEN_SLASH,
+    TOKEN_LPAREN,
+    TOKEN_RPAREN,
+    TOKEN_END,
+    TOKEN_INVALID
+};
+
+struct Token {
+    TokenType type;
+    string value;
+};
+
+//Sets nextToken to next token from input
+
+Token scanToken(const string &input, size_t &pos){
+    Token result;
+    //whitespace skipper
+    while(pos < input.size() && std::isspace(input[pos])){
+        pos++;
+    }
+
+    //find input end
+    if(pos >= input.size()){
+        result.type = TOKEN_END;
+        result.value = "";
+        return result;
+    }
+
+    char currentChar = input[pos];
+
+    //number lexer
+    if(std::isdigit(currentChar)){
+        string num;
+        while(pos < input.size() && std::isdigit(input[pos])){
+            num += input[pos];
+            pos++;
+        }
+        result.type = TOKEN_NUMBER;
+        result.value = num;
+        return result;
+    }
+
+    //operator lexer
+    switch(currentChar){
+        case '+':
+            pos++;
+            result.type = TOKEN_PLUS;
+            result.value = "+";
+            return result;
+        case '-':
+            pos++;
+            result.type = TOKEN_MINUS;
+            result.value = "-";
+            return result;
+        case '*':
+            pos++;
+            result.type = TOKEN_STAR;
+            result.value = "*";
+            return result;
+        case '/':
+            pos++;
+            result.type = TOKEN_SLASH;
+            result.value = "/";
+            return result;
+        case '(':
+            pos++;
+            result.type = TOKEN_LPAREN;
+            result.value = "(";
+            return result;
+        case ')':
+            pos++;
+            result.type = TOKEN_RPAREN;
+            result.value = ")";
+            return result;
+        default: //INSERT LATER ERROR HANDLING
+            pos++;
+            result.type = TOKEN_INVALID;
+            result.value = "ERROR";
+            return result;
+    }
+
+}
+
+int main(){
+    size_t pos = 0;
+    string input = "-(36+5)*2";
+    Token currentToken;
+    do{
+        currentToken = scanToken(input, pos);
+        std::cout << currentToken.value << std::endl;
+    } while(currentToken.value != "");
+
+
+    return 0;
+}
