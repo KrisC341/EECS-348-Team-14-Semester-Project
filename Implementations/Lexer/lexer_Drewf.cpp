7a4ddae66a228f4212704ca8205067120f4b8671
Drew F <143431726+1v9inyourpeak@users.noreply.github.com>
2024-11-13 17:15:10 -0600

    Update lexer_Drewf.cpp (sample input)

--- Implementations/Lexer/lexer_Drewf.cpp
+++ Implementations/Lexer/lexer_Drewf.cpp
@@ -75,7 +75,7 @@ private:
 };
 
 int main() {
-    std::string input = "1 + 2 * (x - 3)**4 /5";  // sample input with exponentiation
+    std::string input = "1 + 2 * (x - 3)**4 /5 % 3";  // sample input
     Lexer lexer(input);
     lexer.tokenize();
 
